08a61a4751a73c75835bbbafef39d17af316456a
Nhu Ha <hamynhu161@gmail.com>
2025-06-08 17:45:32 +0300

    Changed error in setting.py

--- suppliers/settings.py
+++ suppliers/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(%m8em!jg3@fx8jm0b4oxqut5k=5zzysjmedf!elq9ix1+gr0w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
